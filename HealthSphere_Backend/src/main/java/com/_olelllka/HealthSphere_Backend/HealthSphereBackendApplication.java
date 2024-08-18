349f25d8813652a51ef2930a25ac7eb236070e90
Oleshka <olehit32@gmail.com>
2024-08-18 15:40:13 +0200

    Added backend to the project

---
+++ HealthSphere_Backend/src/main/java/com/_olelllka/HealthSphere_Backend/HealthSphereBackendApplication.java
@@ -0,0 +1,13 @@
+package com._olelllka.HealthSphere_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthSphereBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthSphereBackendApplication.class, args);
+	}
+
+}
---
+++ HealthSphere_Backend/src/test/java/com/_olelllka/HealthSphere_Backend/HealthSphereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com._olelllka.HealthSphere_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthSphereBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
