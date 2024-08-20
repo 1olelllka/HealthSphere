b4755f16e640d10fb3c3ad36b4dce3c5b56727ab
Oleshka <olehit32@gmail.com>
2024-08-20 15:54:42 +0200

    Added CORS Policy for frontend interaction

--- HealthSphere_Backend/src/main/java/com/_olelllka/HealthSphere_Backend/configuration/CorsPolicyConfig.java
+++ HealthSphere_Backend/src/main/java/com/_olelllka/HealthSphere_Backend/configuration/CorsPolicyConfig.java
@@ -1,2 +1,20 @@
-package com._olelllka.HealthSphere_Backend.configuration;public class CorsPolicyConfig {
+package com._olelllka.HealthSphere_Backend.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsPolicyConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowCredentials(true).maxAge(3600);
+    }
 }
