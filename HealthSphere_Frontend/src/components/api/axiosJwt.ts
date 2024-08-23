6c4c2d23494aa48ad6db96100cc11c118d5a8b6a
Oleshka <olehit32@gmail.com>
2024-08-23 13:38:59 +0200

    Added function to get jwt token from server

---
+++ HealthSphere_Frontend/src/components/api/axiosJwt.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export default async function getJwtToken(): Promise<string> {
+  try {
+    const response = await axios.get<{ accessToken: string }>(
+      "http://localhost:8000/api/v1/get-jwt",
+      {
+        withCredentials: true,
+      }
+    );
+    console.log(response);
+    return response.data.accessToken;
+  } catch (err) {
+    console.error("Failed to fetch jwt token", err);
+    throw err;
+  }
+}
